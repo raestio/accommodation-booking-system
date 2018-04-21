df0cb5af24de4993ebfaa1fe025f9fbf0db12c52
Rastislav Zlacký <rasto.zlacky@gmail.com>
2018-04-21 12:07:09 +0200

    Hello world UI added

---
+++ app/src/main/java/com/rasto/accommodationreservationsystem/AccommodationReservationSystemApplication.java
@@ -0,0 +1,13 @@
+package com.rasto.accommodationreservationsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccommodationReservationSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccommodationReservationSystemApplication.class, args);
+
+	}
+}
