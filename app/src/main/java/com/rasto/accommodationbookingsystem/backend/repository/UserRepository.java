6e5469a48de8bbeff2dc423809d7e7197c0cf06e
Rastislav Zlacký <rasto.zlacky@gmail.com>
2018-04-24 20:28:25 +0200

    Repositery renamed

--- app/src/main/java/com/rasto/accommodationbookingsystem/backend/repository/UserRepository.java
+++ app/src/main/java/com/rasto/accommodationbookingsystem/backend/repository/UserRepository.java
@@ -3,5 +3,5 @@ package com.rasto.accommodationbookingsystem.backend.repository;
 import com.rasto.accommodationbookingsystem.backend.data.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CustomerRepository extends JpaRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, Long> {
 }
