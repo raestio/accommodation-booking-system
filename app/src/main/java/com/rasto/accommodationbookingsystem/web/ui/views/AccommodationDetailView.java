fdb85ff00ca7487d92b45303eb2ac9cb980a3590
Rastislav Zlacký <rasto.zlacky@gmail.com>
2018-04-26 19:17:20 +0200

    Base booking form added with date picking

--- app/src/main/java/com/rasto/accommodationbookingsystem/web/ui/views/AccommodationDetailView.java
+++ app/src/main/java/com/rasto/accommodationbookingsystem/web/ui/views/AccommodationDetailView.java
@@ -1,25 +1,26 @@
 package com.rasto.accommodationbookingsystem.web.ui.views;
 
 import com.rasto.accommodationbookingsystem.web.ui.MainLayout;
-import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.component.Tag;
+import com.vaadin.flow.component.dependency.HtmlImport;
 import com.vaadin.flow.component.html.Label;
+import com.vaadin.flow.component.polymertemplate.PolymerTemplate;
 import com.vaadin.flow.router.BeforeEvent;
 import com.vaadin.flow.router.HasUrlParameter;
 import com.vaadin.flow.router.Route;
+import com.vaadin.flow.templatemodel.TemplateModel;
 
 @Route(value = "accommodations", layout = MainLayout.class)
-public class AccommodationDetailView extends Div implements HasUrlParameter<Long> {
+@Tag("accommodation-detail")
+@HtmlImport("src/views/accommodation-detail-view.html")
+public class AccommodationDetailView extends PolymerTemplate<TemplateModel> implements HasUrlParameter<Long> {
 
     private Long accommodationId;
     private final Label label = new Label();
 
-    public AccommodationDetailView() {
-        add(label);
-    }
 
     @Override
     public void setParameter(BeforeEvent event, Long parameter) {
         accommodationId = parameter;
-        label.setText("Accommodation: " + accommodationId.toString());
     }
 }
