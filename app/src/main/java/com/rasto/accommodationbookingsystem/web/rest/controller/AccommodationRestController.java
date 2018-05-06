83186262cb4cb73a4c3c306ff6b099f440bc87f6
Rastislav Zlacký <rasto.zlacky@gmail.com>
2018-05-06 15:36:34 +0200

    added GET accommodation cards

--- app/src/main/java/com/rasto/accommodationbookingsystem/web/rest/controller/AccommodationRestController.java
+++ app/src/main/java/com/rasto/accommodationbookingsystem/web/rest/controller/AccommodationRestController.java
@@ -1,19 +1,18 @@
 package com.rasto.accommodationbookingsystem.web.rest.controller;
 
 import com.rasto.accommodationbookingsystem.backend.service.AccommodationService;
+import com.rasto.accommodationbookingsystem.backend.service.dto.AccommodationCardDTO;
 import com.rasto.accommodationbookingsystem.backend.service.dto.AccommodationDTO;
 import com.rasto.accommodationbookingsystem.web.rest.exception.AccommodationNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Collection;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/accommodations")
 public class AccommodationRestController {
 
     private final AccommodationService accommodationService;
@@ -23,12 +22,17 @@ public class AccommodationRestController {
         this.accommodationService = accommodationService;
     }
 
-    @GetMapping
+    @GetMapping(value = "/accommodations")
     public Collection<AccommodationDTO> getAccommodations() {
         return accommodationService.findAllAndConvertToDTO();
     }
 
-    @GetMapping(value = "/{accommodationId}")
+    @GetMapping(value = "/accommodations-cards")
+    public Collection<AccommodationCardDTO> getAccommodationsCards() {
+        return accommodationService.findAllAccommodationsCards();
+    }
+
+    @GetMapping(value = "/accommodations/{accommodationId}")
     public AccommodationDTO getAccommodation(@PathVariable Long accommodationId) {
         Optional<AccommodationDTO> optionalAccommodationDTO = accommodationService.findByIdAndConvertToDTO(accommodationId);
         if (optionalAccommodationDTO.isPresent()) {
